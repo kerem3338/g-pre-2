aa44a3e7a8376c98e536e0bca66baea75e4a29c0
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2020-11-12 15:19:26 +0300

    Add files via upload

---
+++ g.py
@@ -0,0 +1,38 @@
+def alfabe():
+	return "abcçdefgğhıijklmnoöprsştuüvyz"
+
+def yapımcı():
+	return "kerem üstünsoy"
+
+def hesap():
+	print("1")
+	
+def oyun():
+	print("1 2020 2 1990 3 1950")
+	a=input("yıl:")
+	if a =="1":
+		print("doğru")
+	if a =="2":
+	    print("hayır")
+	if a =="3":
+		print("ozam bu bu olmazdı")
+		
+
+def slot():
+    a = input(">>|")
+    b = input(">>|")
+    
+    
+    if a =="alfabe()":
+    	print("abcçdefgğhıijklmnoöprsştuüvyz")
+    if b =="alfabe()":
+    	print("abcçdefgğhıijklmnoöprsştuüvyz")
+    if a =="oyun()":
+        import g
+        
+        print(g.oyun())
+    if a =="yapımcı()":
+    	print("kerem üstünsoy")
+    if b =="yapımcı()":
+    	print("kerem üstünsoy")
+	
---
+++ shell.py
@@ -0,0 +1,14 @@
+import g
+
+while True:
+  sis = input(">>>")
+
+  if sis == "yapımcı()":
+	  print(g.yapımcı())
+	  
+  if sis == "oyun()":
+  	print(g.oyun())
+  if sis =="alfabe()":
+  	print(g.alfabe())
+  if sis =="slot()":
+      print(g.slot())
